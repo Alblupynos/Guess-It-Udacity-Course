28384b792626f8c5cea95d16eaa4c96e328e9670
Albina <alblupynos@gmail.com>
2020-03-29 10:02:44 +0300

    Add LiveData Data Binding.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -53,9 +53,8 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         binding.gameViewModel = viewModel
+        binding.setLifecycleOwner(this)
 
-        viewModel.score.observe(this, Observer { newScore ->  binding.scoreText.text = newScore.toString()})
-        viewModel.word.observe(this, Observer { newWord -> binding.wordText.text = newWord })
         viewModel.currentTime.observe(this, Observer { newTime -> binding.timerText.text = newTime.toString() })
         viewModel.eventGameFinished.observe(this, Observer { hasFinished ->
             if (hasFinished){
